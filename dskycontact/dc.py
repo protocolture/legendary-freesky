d069c4c276ce4739d6c8d6010f6b846522edf2f3
Matt Osborne <osbornematt1@gmail.com>
2023-08-29 19:21:27 +1000

    Update dc.py

--- dskycontact/dc.py
+++ dskycontact/dc.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 import subprocess
 
+HOST="192.168.20.59"
+# Ports are handled in ~/.ssh/config since we use OpenSSH
+COMMAND="sudo python3 /freesky/legendary-freesky/dskyrelay/relaytest3.py"
+
 def main():
+ 
+    
+
+
+ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
+                       shell=False,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+result = ssh.stdout.readlines()
+
     print("This is the stub script for code dc.") 
-    subprocess.Popen(f"ssh -p matt matt@192.168.20.59 python3 /RPi_Relay_Board_B/RaspberryPi/python/relaytest3.py", shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()
+    
+if result == []:
+    error = ssh.stderr.readlines()
+    print >>sys.stderr, "ERROR: %s" % error
+else:
+    print result
+    
 if __name__ == '__main__':
     main()
