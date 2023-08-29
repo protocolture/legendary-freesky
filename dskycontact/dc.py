171e78c1584d6a221804114931e738e9f15e30e7
Matt Osborne <osbornematt1@gmail.com>
2023-08-29 19:51:33 +1000

    Update dc.py

--- dskycontact/dc.py
+++ dskycontact/dc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import subprocess
+import paramiko
+
 
 HOST="192.168.20.59"
 # Ports are handled in ~/.ssh/config since we use OpenSSH
@@ -7,19 +8,15 @@ COMMAND="sudo python3 /freesky/legendary-freesky/dskyrelay/relaytest3.py"
 
 def main():
  
-    ssh = subprocess.Popen(["ssh matt@192.168.109.59 -p matt", "%s" % HOST, COMMAND],
-                       shell=False,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
-    result = ssh.stdout.readlines()
+    ssh = paramiko.SSHClient()
 
     print("This is the stub script for code dc.") 
     
-    if result == []:
-        error = ssh.stderr.readlines()
-        print >>sys.stderr, "ERROR: %s" % error
-    else:
-        print (result)
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ 
+    ssh.connect('192.168.20.59', port=22, username='matt',
+            password='matt', timeout=3)
+    stdin, stdout, stderr = ssh.exec_command('sudo python3 /freesky/legendary-freesky/dskyrelay/relaytest3.py')
     
 if __name__ == '__main__':
     main()
