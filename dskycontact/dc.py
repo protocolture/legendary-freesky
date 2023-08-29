89fcf9dc1833a58d9bf6cb419b1506b11c2e4ab8
Matt Osborne <osbornematt1@gmail.com>
2023-08-29 19:54:13 +1000

    Update dc.py

--- dskycontact/dc.py
+++ dskycontact/dc.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 import paramiko
 
-
-HOST="192.168.20.59"
-# Ports are handled in ~/.ssh/config since we use OpenSSH
-COMMAND="sudo python3 /freesky/legendary-freesky/dskyrelay/relaytest3.py"
-
 def main():
  
     ssh = paramiko.SSHClient()
