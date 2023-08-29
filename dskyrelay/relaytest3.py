954a71e048684887ec76334ec848fd8fd13f0767
Matt Osborne <osbornematt1@gmail.com>
2023-08-29 20:18:04 +1000

    Update relaytest3.py

--- dskyrelay/relaytest3.py
+++ dskyrelay/relaytest3.py
@@ -6,8 +6,10 @@ import time
 
 Relay = [5, 6, 13, 16, 19, 20, 21, 26]
 
+GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD  
 GPIO.setup(Relay[0], GPIO.OUT) # set a port/pin as an output   
 GPIO.output(Relay[0], 1)       # set port/pin value to 1/GPIO.HIGH/True  
-wait (10)
+time.sleep(10)
 GPIO.output(Relay[0], 0)       # set port/pin value to 0/GPIO.LOW/False  
