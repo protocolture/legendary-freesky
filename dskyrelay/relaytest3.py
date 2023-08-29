7172cbb8372c79b17944e1401d0ca2e6a6c1fa4f
Matt Osborne <osbornematt1@gmail.com>
2023-08-29 20:08:01 +1000

    Update relaytest3.py

--- dskyrelay/relaytest3.py
+++ dskyrelay/relaytest3.py
@@ -8,20 +8,13 @@ Relay = [5, 6, 13, 16, 19, 20, 21, 26]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
-for i in range(0,1):
-    GPIO.setup(Relay[0], GPIO.OUT)
-    GPIO.output(Relay[0], GPIO.HIGH)
+GPIO.setup(Relay[0], GPIO.OUT)
+GPIO.output(Relay[0], GPIO.HIGH)
 
 try:
-    while True:
         for i in range(8):
             GPIO.output(Relay[0], GPIO.LOW)
-            time.sleep(1)
-        for i in range(8):
             GPIO.output(Relay[0], GPIO.HIGH)
             time.sleep(10)
-
-
-except:
-    GPIO.cleanup()
+            GPIO.output(Relay[0], GPIO.LOW)
+            
