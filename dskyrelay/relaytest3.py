a2c826c2a30a610e202227a6104a9e220f71244f
Matt Osborne <osbornematt1@gmail.com>
2023-09-17 10:43:53 +1000

    Update relaytest3.py

--- dskyrelay/relaytest3.py
+++ dskyrelay/relaytest3.py
@@ -11,5 +11,6 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD  
 GPIO.setup(Relay[0], GPIO.OUT) # set a port/pin as an output   
 GPIO.output(Relay[0], 1)       # set port/pin value to 1/GPIO.HIGH/True  
-time.sleep(10)
+time.sleep(5)
 GPIO.output(Relay[0], 0)       # set port/pin value to 0/GPIO.LOW/False  
+printf("Smoke Complete")
