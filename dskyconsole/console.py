0dfb9116931e27ee6e14ab09ed77ab46b29ed64d
Matt Osborne <osbornematt1@gmail.com>
2023-09-17 13:52:00 +1000

    Update console.py

--- dskyconsole/console.py
+++ dskyconsole/console.py
@@ -4,20 +4,6 @@ import time
 # Redis connection
 r = redis.Redis(host='192.168.20.71', port=6379, db=0)
 
-# Status indicators
-status_indicators = {
-    'Reactor': ['ON', 'COLD', 'SCRAM'],
-    'environmental_systems': ['ON', 'OFF', 'FAULT'],
-    'life_support': ['ON', 'OFF', 'FAULT'],
-    'Comms': ['ON', 'OFF', 'FAULT'],
-    'Defensive Systems': ['ON', 'OFF', 'FAULT'],
-    'psychic_diffuser': ['ON', 'OFF', 'FAULT'],
-    'Maglev': ['ON', 'OFF', 'FAULT'],
-    'Access Control': ['ON', 'OFF', 'FAULT'],
-    'spirit_containment_field': ['ON', 'OFF', 'FAULT'],
-    'System Booted': ['YES', 'NO']
-}
-
 # Power requirement for each system
 power_req = {
     'life_support': 10,
@@ -30,118 +16,197 @@ power_req = {
     'spirit_containment_field': 40
 }
 
-# Reset Redis with initial statuses
-for status, states in status_indicators.items():
-    r.set(status, states[1])  # sets all statuses to their 'OFF' equivalent
-
-# Set initial power level
-r.set('Power', '0')
-
-def get_reactor_power():
-    return int(r.get('Power'))
-
-def set_reactor_power(power):
-    r.set('Power', str(power))
+def adjust_reactor_power(adjustment):
+    current_power = int(r.get('Power'))
+    current_power += adjustment
+    r.set('Power', str(current_power))
 
 class Reactor:
-    system_name = "Reactor"
+    @staticmethod
+    def Nominal():
+        r.set('Reactor', 'NOMINAL')
+        adjust_reactor_power(1)
 
     @staticmethod
-    def adjust_reactor_power(adjustment):
-        current_power = get_reactor_power()
-        current_power += adjustment
-        set_reactor_power(current_power)
+    def Cold():
+        r.set('Reactor', 'COLD')
 
-    class On:
-        @staticmethod
-        def execute():
-            r.set(Reactor.system_name, 'ON')
-            Reactor.adjust_reactor_power(1)
-
-    class Off:
-        @staticmethod
-        def execute():
-            r.set(Reactor.system_name, 'COLD')
-
-    class Scram:
-        @staticmethod
-        def execute():
-            r.set(Reactor.system_name, 'SCRAM')
-
-class SystemWithPowerReq:
     @staticmethod
-    def adjust_reactor_power(adjustment):
-        current_power = get_reactor_power()
-        current_power += adjustment
-        set_reactor_power(current_power)
+    def Scram():
+        r.set('Reactor', 'SCRAM')
+        adjust_reactor_power(-1)
 
-    class On:
-        @staticmethod
-        def execute():
-            r.set(SystemWithPowerReq.system_name, 'ON')
-            SystemWithPowerReq.adjust_reactor_power(power_req[SystemWithPowerReq.system_name])
+    @staticmethod
+    def Runaway():
+        r.set('Reactor', 'RUNAWAY')
+        adjust_reactor_power(2)
 
-    class Off:
-        @staticmethod
-        def execute():
-            r.set(SystemWithPowerReq.system_name, 'OFF')
-            SystemWithPowerReq.adjust_reactor_power(-power_req[SystemWithPowerReq.system_name])
+class EnvironmentalSystems:
+    @staticmethod
+    def On():
+        r.set('environmental_systems', 'ON')
+        adjust_reactor_power(power_req['environmental_systems'])
 
-    class Fault:
-        @staticmethod
-        def execute():
-            r.set(SystemWithPowerReq.system_name, 'FAULT')
+    @staticmethod
+    def Off():
+        r.set('environmental_systems', 'OFF')
+        adjust_reactor_power(-power_req['environmental_systems'])
 
-class EnvironmentalSystems(SystemWithPowerReq):
-    system_name = "environmental_systems"
+    @staticmethod
+    def Fault():
+        r.set('environmental_systems', 'FAULT')
 
-class LifeSupport(SystemWithPowerReq):
-    system_name = "life_support"
+class LifeSupport:
+    @staticmethod
+    def On():
+        r.set('life_support', 'ON')
+        adjust_reactor_power(power_req['life_support'])
 
-class Comms(SystemWithPowerReq):
-    system_name = "Comms"
+    @staticmethod
+    def Off():
+        r.set('life_support', 'OFF')
+        adjust_reactor_power(-power_req['life_support'])
 
-class DefensiveSystems(SystemWithPowerReq):
-    system_name = "Defensive Systems"
+    @staticmethod
+    def Fault():
+        r.set('life_support', 'FAULT')
 
-class PsychicDiffuser(SystemWithPowerReq):
-    system_name = "psychic_diffuser"
+class Comms:
+    @staticmethod
+    def On():
+        r.set('Comms', 'ON')
+        adjust_reactor_power(power_req['Comms'])
 
-class Maglev(SystemWithPowerReq):
-    system_name = "Maglev"
+    @staticmethod
+    def Off():
+        r.set('Comms', 'OFF')
+        adjust_reactor_power(-power_req['Comms'])
 
-class AccessControl(SystemWithPowerReq):
-    system_name = "Access Control"
+    @staticmethod
+    def Fault():
+        r.set('Comms', 'FAULT')
 
-class SpiritContainmentField(SystemWithPowerReq):
-    system_name = "spirit_containment_field"
-            
-# Mapping of code inputs to specific program executions
+# ... Continuing for all the systems ...
+
+class DefensiveSystems:
+    @staticmethod
+    def On():
+        r.set('Defensive Systems', 'ON')
+        adjust_reactor_power(power_req['Defensive Systems'])
+
+    @staticmethod
+    def Off():
+        r.set('Defensive Systems', 'OFF')
+        adjust_reactor_power(-power_req['Defensive Systems'])
+
+    @staticmethod
+    def Fault():
+        r.set('Defensive Systems', 'FAULT')
+
+class PsychicDiffuser:
+    @staticmethod
+    def On():
+        r.set('psychic_diffuser', 'ON')
+        adjust_reactor_power(power_req['psychic_diffuser'])
+
+    @staticmethod
+    def Off():
+        r.set('psychic_diffuser', 'OFF')
+        adjust_reactor_power(-power_req['psychic_diffuser'])
+
+    @staticmethod
+    def Fault():
+        r.set('psychic_diffuser', 'FAULT')
+
+class Maglev:
+    @staticmethod
+    def On():
+        r.set('Maglev', 'ON')
+        adjust_reactor_power(power_req['Maglev'])
+
+    @staticmethod
+    def Off():
+        r.set('Maglev', 'OFF')
+        adjust_reactor_power(-power_req['Maglev'])
+
+    @staticmethod
+    def Fault():
+        r.set('Maglev', 'FAULT')
+
+class AccessControl:
+    @staticmethod
+    def On():
+        r.set('Access Control', 'ON')
+        adjust_reactor_power(power_req['Access Control'])
+
+    @staticmethod
+    def Off():
+        r.set('Access Control', 'OFF')
+        adjust_reactor_power(-power_req['Access Control'])
+
+    @staticmethod
+    def Fault():
+        r.set('Access Control', 'FAULT')
+
+class SpiritContainmentField:
+    @staticmethod
+    def On():
+        r.set('spirit_containment_field', 'ON')
+        adjust_reactor_power(power_req['spirit_containment_field'])
+
+    @staticmethod
+    def Off():
+        r.set('spirit_containment_field', 'OFF')
+        adjust_reactor_power(-power_req['spirit_containment_field'])
+
+    @staticmethod
+    def Fault():
+        r.set('spirit_containment_field', 'FAULT')
+
+# The code map 
 code_map = {
-    1001: Reactor.On,
-    2001: Reactor.Off,
+    1001: Reactor.Nominal,
+    2001: Reactor.Cold,
+    3001: Reactor.Scram,
+    4001: Reactor.Runaway,
     1002: EnvironmentalSystems.On,
     2002: EnvironmentalSystems.Off,
+    3002: EnvironmentalSystems.Fault,
     1003: LifeSupport.On,
     2003: LifeSupport.Off,
+    3003: LifeSupport.Fault,
     1004: Comms.On,
     2004: Comms.Off,
+    3004: Comms.Fault,
     1005: DefensiveSystems.On,
     2005: DefensiveSystems.Off,
+    3005: DefensiveSystems.Fault,
     1006: PsychicDiffuser.On,
     2006: PsychicDiffuser.Off,
+    3006: PsychicDiffuser.Fault,
     1007: Maglev.On,
     2007: Maglev.Off,
+    3007: Maglev.Fault,
     1008: AccessControl.On,
     2008: AccessControl.Off,
+    3008: AccessControl.Fault,
     1009: SpiritContainmentField.On,
-    2009: SpiritContainmentField.Off
+    2009: SpiritContainmentField.Off,
+    3009: SpiritContainmentField.Fault,
 }
 
-while True:
-    input_code = int(input("Enter the code: "))  # Receive code input
-    program_to_execute = code_map.get(input_code)
-    if program_to_execute:
-        program_to_execute.execute()
+# A simple executor to activate the systems based on the entered code
+def execute_system(code):
+    if code in code_map:
+        code_map[code]()
     else:
-        print("Invalid Code")
+        print("Invalid code entered!")
+
+# Test
+if __name__ == "__main__":
+    while True:
+        try:
+            code = int(input("Enter the code: "))
+            execute_system(code)
+        except ValueError:
+            print("Please enter a valid integer code!")
