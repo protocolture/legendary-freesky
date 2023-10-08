33da2aa2702cf56de81cdf5ac322190cef270eea
Matt Osborne <osbornematt1@gmail.com>
2023-10-08 21:55:09 +1000

    Update cb.py

--- dskycontact/cb.py
+++ dskycontact/cb.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+import redis
+
 def main():
-    print("Red Keycard")
+    # Connect to Redis
+    r = redis.Redis(host='192.168.20.71', port=6379, db=0)
+
+    # Set GOSMOKE to True (1) and Smokeseconds to desired value (e.g., 5 seconds)
+    r.set('GOSMOKE', '1')
+    r.set('Smokeseconds', '5')
 
 if __name__ == '__main__':
     main()
