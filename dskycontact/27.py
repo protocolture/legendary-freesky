88a6308ee281092127639452ed72c319d94c70cc
Matt Osborne <osbornematt1@gmail.com>
2023-10-08 22:29:33 +1000

    Update 27.py

--- dskycontact/27.py
+++ dskycontact/27.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+import redis
+
 def main():
-    print("Yellow Keycard")
+    # Connect to Redis
+    r = redis.Redis(host='192.168.20.71', port=6379, db=0)
+    print("SMoooookin")
+    # Set GOSMOKE to True (1) and Smokeseconds to desired value (e.g., 5 seconds)
+    r.set('GOSMOKE', '1')
+    r.set('Smokeseconds', '5')
+    r.set('SC1', '1')
+    r.set('SCSeconds', '5')
 
 if __name__ == '__main__':
     main()
