e8f2686345527ec1f0386f09cfed9edd2f68917f
Matt Osborne <osbornematt1@gmail.com>
2023-10-08 22:40:35 +1000

    Update 27.py

--- dskycontact/27.py
+++ dskycontact/27.py
@@ -5,10 +5,12 @@ import redis
 def main():
     # Connect to Redis
     r = redis.Redis(host='192.168.20.71', port=6379, db=0)
-    print("SMoooookin")
+    print("Yellow Card")
     # Set GOSMOKE to True (1) and Smokeseconds to desired value (e.g., 5 seconds)
     r.set('GOSMOKE', '1')
+    print("SMOKE for 5")
     r.set('Smokeseconds', '5')
+    print("Short for 5")
     r.set('SC1', '1')
     r.set('SCSeconds', '5')
 
