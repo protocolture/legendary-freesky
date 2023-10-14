e4400bbf3bf05884e99b44d0eb29a39a27a8d1d3
Matt Osborne <osbornematt1@gmail.com>
2023-10-14 22:33:40 +1000

    Update printer.py

--- dskycompute/printer.py
+++ dskycompute/printer.py
@@ -10,7 +10,7 @@ p = Usb(0x04b8, 0x0202, 0)
 # Load and print the image
 image_path = "ports/port8.jpg"
 original_image = Image.open(image_path)
-target_width_mm = 72  # ~90% of 80mm
+target_width_mm = 40  # ~90% of 80mm
 target_width_px = int((target_width_mm / 25.4) * 203)  # 203dpi is a common print density for thermal printers
 
 # Resize image to fit width of paper
@@ -27,10 +27,14 @@ with open(char_file_path, "r") as file:
     char_details = file.read()
 
 # Format and print the text
-p.set(align="center", type="B", height=2, width=2)  # Bold, double height and width for Name
+p.set(align="center")
+p.set_text_size(2, 2)  # double width and height
 p.text(char_details.split('\n')[0] + "\n")  # Print name
+p.set_text_size(1, 1)  # Reset to normal size for rest
 
-p.set(align="left", type="B", height=1, width=1)  # Reset to normal size for rest
+p.set(align="left")
+for line in wrapped_text[1:]:
+    p.text(line + "\n") # Reset to normal size for rest
 wrapped_text = textwrap.wrap(char_details, width=32)  # Wrap text at approx 32 chars per line
 
 # Skip printing the name again
