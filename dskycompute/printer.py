0060105ee709d8f63055cf6ef5b7090bae7de5f0
Matt Osborne <osbornematt1@gmail.com>
2023-10-14 22:28:28 +1000

    updates

--- dskycompute/printer.py
+++ dskycompute/printer.py
@@ -7,39 +7,38 @@ from escpos.printer import Usb
 # You might find these using "lsusb" command in Linux
 p = Usb(0x04b8, 0x0202, 0)
 
-try:
-    # Set text size
-    p.set(font='a', height=1, width=1)
+# Load and print the image
+image_path = "ports/port8.jpg"
+original_image = Image.open(image_path)
+target_width_mm = 72  # ~90% of 80mm
+target_width_px = int((target_width_mm / 25.4) * 203)  # 203dpi is a common print density for thermal printers
 
-    # Print text
-    p.text("Hello, here is your receipt with an image!\n")
+# Resize image to fit width of paper
+w_percent = (target_width_px / float(original_image.size[0]))
+target_height_px = int((float(original_image.size[1]) * float(w_percent)))
+resized_image = original_image.resize((target_width_px, target_height_px), Image.ANTIALIAS)
 
-    # Load and resize the image
-    base_path = os.path.dirname(os.path.abspath(__file__))  # Get script directory
-    image_path = os.path.join(base_path, "ports", "port8.jpg")
-    original_image = Image.open(image_path)
-    
-    # Assuming 80mm wide paper, with 203DPI typical on Epson printers
-    # Resizing to 90% of paper width
-    target_width_mm = 40
-    dpi = 203
-    target_width_px = int((target_width_mm / 25.4) * dpi)
-    
-    # Maintain aspect ratio during resize
-    w_percent = (target_width_px / float(original_image.size[0]))
-    target_height_px = int((float(original_image.size[1]) * float(w_percent)))
-    
-    resized_image = original_image.resize((target_width_px, target_height_px), Image.ANTIALIAS)
-    
-    # Print image
-    p.image(resized_image)
-    
-    # Additional text
-    p.text("Here's more text after the image.\n")
-    
-    # Cut the paper
-    p.cut()
+# Print image
+p.image(resized_image)
 
-finally:
-    # Ensure the device is properly reset for next print
-    p.device.close()
+# Load character details
+char_file_path = "chars/char1.txt"
+with open(char_file_path, "r") as file:
+    char_details = file.read()
+
+# Format and print the text
+p.set(align="center", type="B", height=2, width=2)  # Bold, double height and width for Name
+p.text(char_details.split('\n')[0] + "\n")  # Print name
+
+p.set(align="left", type="B", height=1, width=1)  # Reset to normal size for rest
+wrapped_text = textwrap.wrap(char_details, width=32)  # Wrap text at approx 32 chars per line
+
+# Skip printing the name again
+for line in wrapped_text[1:]:
+    p.text(line + "\n")
+
+# Ensure to cut the paper after printing
+p.cut()
+
+# Close the connection to the printer
+p.close()
