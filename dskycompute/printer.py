72ab32979040ad5431f3408d7ce30faf3fb299f6
Matt Osborne <osbornematt1@gmail.com>
2023-10-14 22:20:05 +1000

    Update printer.py

--- dskycompute/printer.py
+++ dskycompute/printer.py
@@ -1,23 +1,40 @@
+import os
+from PIL import Image
 from escpos.printer import Usb
 
-# Printer setup
-# Replace Vendor id and Product id with the values for your printer
-# You might find these using "lsusb" command in Linux
-p = Usb(0x04b8, 0x0202, 0)
+# Setup
+p = Usb(0x04b8, 0x0e15, 0)
 
 try:
-    # Set the text size: 1x size for both vertical and horizontal
-    # This can be adjusted as per need
+    # Set text size
     p.set(font='a', height=1, width=1)
 
-    # Text output
-    p.text("Hello, this is your Epson printer speaking!\n")
-    p.text("Here's a line of small text.\n")
-    
-    # Set the text size: 2x size for both vertical and horizontal
-    p.set(height=2, width=2)
-    p.text("And here is bigger text!\n")
+    # Print text
+    p.text("Hello, here is your receipt with an image!\n")
 
+    # Load and resize the image
+    base_path = os.path.dirname(os.path.abspath(__file__))  # Get script directory
+    image_path = os.path.join(base_path, "ports", "port8.jpg")
+    original_image = Image.open(image_path)
+    
+    # Assuming 80mm wide paper, with 203DPI typical on Epson printers
+    # Resizing to 90% of paper width
+    target_width_mm = 72
+    dpi = 203
+    target_width_px = int((target_width_mm / 25.4) * dpi)
+    
+    # Maintain aspect ratio during resize
+    w_percent = (target_width_px / float(original_image.size[0]))
+    target_height_px = int((float(original_image.size[1]) * float(w_percent)))
+    
+    resized_image = original_image.resize((target_width_px, target_height_px), Image.ANTIALIAS)
+    
+    # Print image
+    p.image(resized_image)
+    
+    # Additional text
+    p.text("Here's more text after the image.\n")
+    
     # Cut the paper
     p.cut()
 
