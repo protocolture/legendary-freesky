e91c7e50779f11cb421208471cb28818aac4a8f2
Matt Osborne <osbornematt1@gmail.com>
2023-10-22 21:23:18 +1000

    KASA to REDIS

---
+++ dskybrain/POWERUP.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+kasa --type strip --host 192.168.20.148 on --index 1
+sleep 2
+kasa --type strip --host 192.168.20.148 on --index 0
+kasa --type strip --host 192.168.20.63 on --index 0
+kasa --type strip --host 192.168.20.64 on --index 0
+kasa --type strip --host 192.168.20.64 on --index 1
+kasa --type strip --host 192.168.20.64 on --index 2
+sleep 2
+kasa --type strip --host 192.168.20.148 off --index 1
+
