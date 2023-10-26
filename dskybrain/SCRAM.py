59d55bdfde57a791172f93e16baed2611b77dc90
Matt Osborne <osbornematt1@gmail.com>
2023-10-26 19:28:19 +1000

    updates

--- dskybrain/SCRAM.py
+++ dskybrain/SCRAM.py
@@ -10,12 +10,13 @@ def main():
     # Connect to Redis
     r = redis.Redis(host='192.168.20.71', port=6379, db=0)
     print("SMOKE for 8")
+    r.set('GOSMOKE', '8')
     r.set('Smokeseconds', '8')
     print("Short for 8")
     r.set('SC1', '8')
     r.set('SCSeconds', '8')
-    cmd = "kasa --type strip --host 192.168.20.148 on --index 1"
-    subprocess.run(cmd, shell=True)
+    #cmd = "kasa --type strip --host 192.168.20.148 on --index 1"
+    #subprocess.run(cmd, shell=True)
     #bash_script_path = 'SCRAM.sh'  # Replace with the actual path to your Bash script
     #subprocess.Popen(['bash', bash_script_path])
 
