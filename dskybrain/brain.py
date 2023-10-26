823e15d1b0d2a15d38d729fc82635d61099a2e43
Matt Osborne <osbornematt1@gmail.com>
2023-10-26 22:53:41 +1000

    noises

--- dskybrain/brain.py
+++ dskybrain/brain.py
@@ -2,48 +2,58 @@ import redis
 import time
 import subprocess
 import os
+from pygame import mixer
 
 # Configuration for Redis
-redis_host = '192.168.20.71'  # Change this to your Redis server's host
-redis_port = 6379  # Change this to your Redis server's port
-keys_to_check = ['SCRAM', 'POWERUP']  # List of keys to check
-
-# Directory where the script is located
-script_directory = os.path.dirname(os.path.realpath(__file__))
-script_name = os.path.basename(__file__)
+redis_host = '192.168.20.71'
+redis_port = 6379
 
 # Initialize the Redis client
 redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
 
-def check_and_set_values():
-    # Check if RED, BLUE, and YELLOW are all set to "1"
-    if redis_client.get('RED') == '1' and redis_client.get('BLUE') == '1' and redis_client.get('YELLOW') == '1':
-        # Set the other values if the conditions are met
-        redis_client.set('GOSMOKE', '1')
-        redis_client.set('smokeseconds', '12')
-        redis_client.set('POWERUP', '1')
-        # Reset RED, BLUE, and YELLOW to "0"
-        redis_client.set('RED', '0')
-        redis_client.set('BLUE', '0')
-        redis_client.set('YELLOW', '0')
-        print("Values set and RED, BLUE, YELLOW reset!")
+# Initialize pygame mixer
+mixer.init()
+
+def play_sound(filename):
+    mixer.music.load(filename)
+    mixer.music.play()
+    while mixer.music.get_busy():
+        time.sleep(0.1)
 
 while True:
-    # Check and set values for RED, BLUE, YELLOW conditions
-    check_and_set_values()
-    
-    for key in keys_to_check:
-        
-        # Check each key in the list
-        key_value = redis_client.get(key)
-        print(f"{key}, {key_value}")
-        if key_value == '1':
-            print(f"Launching {key}.py")
-            
-            # Launch the script with the same name as this script
-            subprocess.Popen(['python', os.path.join(script_directory, f"{key}.py")])
-            redis_client.set('key', '0')
-    
-    # Sleep for a while before checking again
-    # Adjust the sleep duration as needed
+    # Check for color keys
+    red = redis_client.get("RED")
+    blue = redis_client.get("BLUE")
+    yellow = redis_client.get("YELLOW")
+
+    if red == '1' and blue == '1' and yellow == '1':
+        redis_client.set("GOSMOKE", 1)
+        redis_client.set("smokeseconds", 12)
+        redis_client.set("POWERUP", 1)
+        redis_client.set("RED", 0)
+        redis_client.set("BLUE", 0)
+        redis_client.set("YELLOW", 0)
+
+    # Check for POWERUP and SCRAM keys
+    powerup = redis_client.get("POWERUP")
+    scram = redis_client.get("SCRAM")
+
+    if powerup == '1':
+        subprocess.run(["python", "POWERUP.py"])  # Replace with your actual script name
+        redis_client.set("POWERUP", 0)
+    if scram == '1':
+        subprocess.run(["python", "SCRAM.py"])  # Replace with your actual script name
+        redis_client.set("SCRAM", 0)
+
+    # Check for journal and alarm keys
+    for i in range(1, 11):  # For journal1 to journal10
+        journal_key = f"journal{i}"
+        if redis_client.get(journal_key) == '1':
+            play_sound(f"noise/journal{i}.mp3")
+            redis_client.set(journal_key, 0)
+
+    if redis_client.get("alarm") == '1':
+        play_sound("noise/alarm.mp3")
+        redis_client.set("alarm", 0)
+
     time.sleep(1)
