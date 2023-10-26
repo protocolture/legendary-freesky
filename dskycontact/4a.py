71f3abf7afdcff44b508e1af2d94179fe77549b0
Matt Osborne <osbornematt1@gmail.com>
2023-10-26 21:50:25 +1000

    sms

---
+++ dskycontact/4a.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import redis
+import time
+
+def main():
+	# Connect to Redis
+	r = redis.Redis(host='192.168.20.71', port=6379, db=0)
+
+	# Set char1 to 1
+	r.set('sm2', '1')
+
+    
+if __name__ == '__main__':
+    main()
