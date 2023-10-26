50c6ef87bdd987aa316698df9ce5b729511e96f6
Matt Osborne <osbornematt1@gmail.com>
2023-10-26 21:58:29 +1000

    asd

--- dskycontact/9b.py
+++ dskycontact/9b.py
@@ -5,9 +5,9 @@ import time
 def main():
 	# Connect to Redis
 	r = redis.Redis(host='192.168.20.71', port=6379, db=0)
-
+	print("redrum")
 	# Set char1 to 1
-	
+	r.set('note3', '1')
 
     
 if __name__ == '__main__':
