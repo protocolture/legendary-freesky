e63beef5977dbd81d6a21a5c9662ae131be18e4e
Matt Osborne <osbornematt1@gmail.com>
2023-11-04 02:17:05 +1000

    Update bc.py

--- dskycontact/bc.py
+++ dskycontact/bc.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+import redis
+import time
 
 def main():
-    print("This is the stub script for code bc.")
+	# Connect to Redis
+	r = redis.Redis(host='192.168.20.71', port=6379, db=0)
 
+	# Set char1 to 1
+	r.set('journal11', '1')
+
+    
 if __name__ == '__main__':
     main()
