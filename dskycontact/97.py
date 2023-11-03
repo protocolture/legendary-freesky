f2adc143bce01ab45dc54a37297a243c37c9ee7a
Matt Osborne <osbornematt1@gmail.com>
2023-11-03 17:48:36 +1000

    Update 97.py

--- dskycontact/97.py
+++ dskycontact/97.py
@@ -2,6 +2,9 @@
 
 def main():
     print("This is the stub script for code 9c.")
+    # Connect to Redis
+    r = redis.Redis(host='192.168.20.71', port=6379, db=0)
+
     # Set char1 to 1
     r.set('GOSMOKE', '1')
     r.set('smokeseconds', '600')
