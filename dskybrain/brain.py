373fda41ee5b5a4df83d4f4254c3b1c39ddcc583
Matt Osborne <osbornematt1@gmail.com>
2023-11-03 18:42:58 +1000

    Rant test

--- dskybrain/brain.py
+++ dskybrain/brain.py
@@ -16,6 +16,9 @@ redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_respon
 pygame.init()
 mixer.init()
 
+# Initialize a set to keep track of played rants
+played_rants = set()
+
 def play_sound(filename, volume=1.0):  # volume can range from 0.0 to 1.0
     mixer.music.load(filename)
     print("Playing Sound")
@@ -23,12 +26,29 @@ def play_sound(filename, volume=1.0):  # volume can range from 0.0 to 1.0
     mixer.music.play()
     while mixer.music.get_busy():
         time.sleep(0.1)
+        
+def play_random_rant():
+    global played_rants
+    available_rants = set(f"rant{i}" for i in range(1, 13)) - played_rants
+    if available_rants:
+        rant_to_play = random.choice(list(available_rants))
+        play_sound(os.path.join("noise", f"{rant_to_play}.mp3"))
+        played_rants.add(rant_to_play)
+        print(f"Played: {rant_to_play}")
+    else:
+        print("All rants have been played.")
+
+
 
 while True:
     # Check for color keys
     red = redis_client.get("RED")
     blue = redis_client.get("BLUE")
     yellow = redis_client.get("YELLOW")
+    
+    if redis_client.get("RANT") == '1':
+    play_random_rant()
+    redis_client.set("RANT", 0)
 
     if red == '1' and blue == '1' and yellow == '1':
         redis_client.set("GOSMOKE", 1)
