50e9ac8c3e44f2cba1e48157dcf795e66024475c
Matt Osborne <osbornematt1@gmail.com>
2023-11-04 00:42:11 +1000

    Update d7.py

--- dskycontact/d7.py
+++ dskycontact/d7.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-import paramiko
+import redis
+import time
 
 def main():
- 
-    ssh = paramiko.SSHClient()
+    print("This is the stub script for code d7.")
+    # Connect to Redis
+    r = redis.Redis(host='192.168.20.71', port=6379, db=0)
 
-    print("This is the stub script for code dc.") 
-    
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
- 
-    ssh.connect('192.168.20.59', port=22, username='matt',
-            password='matt', timeout=3)
-    stdin, stdout, stderr = ssh.exec_command('sudo python3 /freesky/legendary-freesky/dskyrelay/relaytest3.py')
+    # Set char1 to 1
+    r.set("journal12", 1)
     
 if __name__ == '__main__':
     main()
