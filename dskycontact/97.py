f02f35d66f47f65cb9fdd62e23c7e0e0ea8f4979
Matt Osborne <osbornematt1@gmail.com>
2023-11-03 17:47:19 +1000

    Update 97.py

--- dskycontact/97.py
+++ dskycontact/97.py
@@ -2,6 +2,9 @@
 
 def main():
     print("This is the stub script for code 9c.")
+    # Set char1 to 1
+    r.set('GOSMOKE', '1')
+    r.set('smokeseconds', '600')
 
 if __name__ == '__main__':
     main()
