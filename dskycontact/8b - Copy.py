e49adfbd56aa1498e4339c90322bc5124fae4a79
Matt Osborne <osbornematt1@gmail.com>
2023-11-04 01:42:14 +1000

    asd

--- dskycontact/8b - Copy.py
+++ dskycontact/8b - Copy.py
@@ -5,9 +5,8 @@ import time
 def main():
 	# Connect to Redis
 	r = redis.Redis(host='192.168.20.71', port=6379, db=0)
-
 	# Set char1 to 1
-	r.set('journal5', '1')
+	r.set('note2', '1')
 
     
 if __name__ == '__main__':
