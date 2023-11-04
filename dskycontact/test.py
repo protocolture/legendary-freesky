5f207959a9fa78747ea6a77e6ac72fac031798f4
Matt Osborne <osbornematt1@gmail.com>
2023-11-04 11:49:13 +1000

    Update test.py

--- dskycontact/test.py
+++ dskycontact/test.py
@@ -7,7 +7,7 @@ def main():
 	r = redis.Redis(host='192.168.20.71', port=6379, db=0)
 
 	# Set char1 to 1
-        r.set('smokeseconds', '2')
+	r.set('smokeseconds', '2')
 	r.set('GOSMOKE', '1')
 
     
